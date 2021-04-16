5bd18caaf25e5ddb88db0a86cbaadb4794757ea1
Hongyi Chen <34770590+nonococoleo@users.noreply.github.com>
2021-04-16 15:57:39 +0800

    DistilBERT Base model modified for classification

---
+++ BERT.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+from transformers import DistilBertModel
+
+
+class BERTForClassification(nn.Module):
+    def __init__(self, bert_model, num_class):
+        super(BERTForClassification, self).__init__()
+
+        self.bert_layer = bert_model
+
+        # Freeze bert layers
+        for p in self.bert_layer.parameters():
+            p.requires_grad = False
+
+        # Classification layer
+        self.fc = nn.Linear(768, num_class)
+
+    def forward(self, sequence, attention_masks):
+        # Feeding the input to BERT model to obtain contextualized representations
+        outputs = self.bert_layer(sequence, attention_mask=attention_masks)
+
+        # Use hidden states of [CLS] for classification
+        out = outputs.last_hidden_state[:, 0, :]
+
+        # Feeding cls_rep to the classifier layer
+        out = self.fc(out)
+
+        return out
+
+
+def get_model(num_class=2, state_dict_file=None, model_name='distilbert-base-uncased'):
+    if not state_dict_file:
+        bert = DistilBertModel.from_pretrained(model_name)
+    else:
+        bert = DistilBertModel()
+        state_dict = torch.load(state_dict_file)
+        bert.load_state_dict(state_dict)
+
+    return BERTForClassification(bert, num_class)
