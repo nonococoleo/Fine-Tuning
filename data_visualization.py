cf0f2a2e3db02dd0f8596ad35bf2d80157e331df
Hongyi Chen <34770590+nonococoleo@users.noreply.github.com>
2021-05-05 16:08:47 +0800

    cleanup

--- data_visualization.py
+++ data_visualization.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python
-
 import os
 import matplotlib.pyplot as plt
 
 
 def isplit_by_n(ls, n):
     for i in range(0, len(ls), n):
-        if i+n >= len(ls):
-            yield sum(ls[i:-1])/(len(ls)-i)
+        if i + n >= len(ls):
+            yield sum(ls[i:-1]) / (len(ls) - i)
         else:
-            yield sum(ls[i:i+n])/n
+            yield sum(ls[i:i + n]) / n
+
 
 def split_by_n(ls, n):
     return list(isplit_by_n(ls, n))
@@ -20,7 +19,7 @@ def plot_bs():
     file_list = os.listdir(path)
     for file in file_list:
         if file.endswith('.out'):
-            f = open(path+file, 'r')
+            f = open(path + file, 'r')
             data = [float(line.rstrip()) for line in f]
             f.close()
 
@@ -34,16 +33,16 @@ def plot_bs():
     plt.ylabel('Loss')
     plt.xlabel('Step (%)')
     plt.legend()
-    plt.ylim(0.25,0.5)
+    plt.ylim(0.20, 0.5)
     plt.show()
-    
-    
+
+
 def plot_lr():
     path = os.getcwd()+'/agnews/lr/'
     file_list = os.listdir(path)
     for file in file_list:
         if file.endswith('.out'):
-            f = open(path+file, 'r')
+            f = open(path + file, 'r')
             data = [float(line.rstrip()) for line in f]
             f.close()
 
@@ -52,16 +51,16 @@ def plot_lr():
             y = avg_data
             x = [i / n * 100 for i in range(1, n + 1)]
             label = file[-7:-4]
-            label = label[:2]+'-'+label[2:]
+            label = label[:2] + '-' + label[2:]
             plt.plot(x, y, label=label)
 
     plt.title('Training Loss vs Step by different Learning Rate')
     plt.ylabel('Loss')
     plt.xlabel('Step (%)')
     plt.legend()
-    plt.ylim(0.2,1)
+    plt.ylim(0.2, 1)
     plt.show()
-    
+
 
 if __name__ == '__main__':
     plot_bs()
--- finetune.py
+++ finetune.py
@@ -10,7 +10,7 @@ from ClassificationDataset import ClassificationDataset
 
 import argparse
 
-parser = argparse.ArgumentParser(description='fine-tune pretrained BERT model')
+parser = argparse.ArgumentParser(description='fine-tune pre-trained BERT model')
 
 parser.add_argument('--num_worker', default=1, type=int,
                     help='Number of workers for dataloader')
@@ -24,20 +24,20 @@ parser.add_argument('-p', '--pretrain_state_dict_file', default=None, type=str,
 parser.add_argument('-s', '--start_epoch', default=1, type=int,
                     help='Start epoch')
 parser.add_argument('-e', '--num_epoch', default=5, type=int,
-                    help='Number of epoch used to finetune')
+                    help='Number of epoch used to fine-tune')
 
 parser.add_argument('-d', '--dataset_name', default="yelp", type=str,
                     help='Dataset used to fine-tune')
 parser.add_argument('-c', '--num_class', default=2, type=int,
                     help='Number of classes in the dataset')
 parser.add_argument('-l', '--sent_length', default=510, type=int,
-                    help='Sent length used to finetune')
+                    help='Sent length used to fine-tune')
 parser.add_argument('-b', '--batch_size', default=32, type=int,
-                    help='Batch size used to finetune')
+                    help='Batch size used to fine-tune')
 parser.add_argument('-r', '--learning_rate', default=2e-3, type=float,
-                    help='Learning rate used to finetune')
+                    help='Learning rate used to fine-tune')
 parser.add_argument('-w', '--warmup_proportion', default=0.1, type=float,
-                    help='Warmup Proportion used to finetune')
+                    help='Warmup Proportion used to fine-tune')
 
 args = parser.parse_args()
 
